f60fcfa5425b792dddd262a6cd8f0237135a1a3e
Guy Halse <guy@tenet.ac.za>
2021-03-30 09:34:13 +0200

    Make tokens live in /softhsm

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM php:7-fpm AS goeleven
 
 RUN apt-get update \
-    && apt-get install -y --no-install-recommends opensc softhsm2 libsofthsm2 libengine-pkcs11-openssl libltdl-dev p11-kit golang-go \
+    && apt-get install -y --no-install-recommends opensc softhsm2 libsofthsm2 libengine-pkcs11-openssl p11-kit gettext golang-go libltdl-dev \
     && rm -rf /var/lib/apt/lists/*
 
 ADD https://github.com/tenet-ac-za/goeleven/archive/master.tar.gz /opt/
@@ -17,6 +17,8 @@ ENV GOPATH=/opt/goeleven
 WORKDIR /opt/goeleven
 RUN go build src/goeleven/goeleven.go
 
+COPY softhsm2.conf /opt/goeleven
+COPY openssl.cnf.tmpl /opt/goeleven
 COPY entrypoint.sh /
 
 EXPOSE 8080/tcp
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,18 +8,32 @@ export SOFTHSM_CONF=/etc/softhsm/softhsm2.conf
 # See https://golang.org/doc/go1.6#cgo
 export GODEBUG=cgocheck=0
 
+# expected to be externally configured
 : ${GOELEVEN_SECRET:=changemeplease}
 : ${GOELEVEN_LABEL:=docker1}
 : ${GOELEVEN_KEY_BITS:=3072}
 
+# really internal config, but could be set by advanced users
+: ${SOFTHSM2_CONF:=/opt/goeleven/softhsm2.conf}
 : ${GOELEVEN_SLOT_PASSWORD:=${GOELEVEN_SECRET}}
 : ${GOELEVEN_KEY_LABEL:=${GOELEVEN_LABEL}:${GOELEVEN_SECRET}}
 : ${GOELEVEN_STATUS_KEY:=${GOELEVEN_LABEL}}
 
-export GOELEVEN_SLOT_PASSWORD GOELEVEN_KEY_LABEL GOELEVEN_STATUS_KEY
+export GOELEVEN_LABEL GOELEVEN_KEY_BITS SOFTHSM2_CONF GOELEVEN_SLOT_PASSWORD GOELEVEN_KEY_LABEL GOELEVEN_STATUS_KEY
+
+if [ -e "${SOFTHSM2_CONF}" ] ; then
+    TOKENDIR=$(awk -F '[[:space:]]*=[[:space:]]*' '/directories.tokendir/ {print $2}' "${SOFTHSM2_CONF}")
+    echo "Getting token directory from ${SOFTHSM2_CONF}: ${TOKENDIR}"
+    : ${TOKENDIR:=/softhsm/tokens/}
+    mkdir -p "${TOKENDIR}"
+    chown -R root:softhsm "${TOKENDIR}"
+    chmod 2770 "${TOKENDIR}" $(dirname "${TOKENDIR%%/}")
+else
+    echo "ERROR: Unknown softhsm2 config"
+    exit 1;
+fi
 
 INITIALISED=$(softhsm2-util --show-slots | awk "/Label:.*${GOELEVEN_LABEL}/ { print \$2 }")
-
 if [ "${INITIALISED}" != "${GOELEVEN_LABEL}" ] ; then
     echo "Initialise a new HSM and generate a key"
     softhsm2-util --init-token --free --label "${GOELEVEN_LABEL}" --pin "${GOELEVEN_SLOT_PASSWORD}" --so-pin "${GOELEVEN_SLOT_PASSWORD}"
@@ -29,49 +43,25 @@ fi
 
 export GOELEVEN_SLOT=$(softhsm2-util --show-slots --label "${GOELEVEN_LABEL}" | awk '/^Slot / { print $2; exit }')
 export GOELEVEN_SERIALNUMBER=$(softhsm2-util --show-slots --label "${GOELEVEN_LABEL}" | awk '/Serial number:/ { print $3; exit }')
+export ARCH=$(uname -m)
 
-cat <<- EOM > /opt/goeleven/openssl.cfg
-openssl_conf = openssl_init
+envsubst < /opt/goeleven/openssl.cnf.tmpl > /opt/goeleven/openssl.cnf
+export OPENSSL_CONF=/opt/goeleven/openssl.cnf
 
-[openssl_init]
-engines = engine_section
-
-[ req ]
-default_bits = ${GOELEVEN_KEY_BITS}
-default_md = sha256
-string_mask = utf8only
-x509_extensions = v3_ca
-prompt = no
-distinguished_name = req_distinguished_name
-
-[ req_distinguished_name ]
-countryName = ZA
-commonName = ${GOELEVEN_LABEL}
-
-[ v3_ca ]
-subjectKeyIdentifier=hash
-keyUsage = nonRepudiation, digitalSignature, keyEncipherment
-authorityKeyIdentifier=keyid:always,issuer
-issuerAltName=issuer:copy
-basicConstraints = critical,CA:false
-
-[engine_section]
-pkcs11 = pkcs11_section
-
-[pkcs11_section]
-dynamic_path = /usr/lib/$(uname -m)-linux-gnu/engines-1.1/pkcs11.so
-MODULE_PATH = ${GOELEVEN_HSMLIB}
-init = ${GOELEVEN_SLOT}
-EOM
-export OPENSSL_CONF=/opt/goeleven/openssl.cfg
-
+# create the cert if it doesn't exit
 if [ \! -e "/opt/goeleven/${GOELEVEN_LABEL}.der" ] ; then
     echo "Generate a new certifictate"
     openssl req -new -x509 -engine pkcs11 -keyform engine -key "${GOELEVEN_SLOT}:01" -passin env:GOELEVEN_SLOT_PASSWORD -days 5479 -sha256 -subj "/C=ZA/CN=${GOELEVEN_LABEL}" -outform der -out "/opt/goeleven/${GOELEVEN_LABEL}.der"
-    pkcs11-tool --module ${GOELEVEN_HSMLIB} --label "${GOELEVEN_LABEL}" --id 01 --login --pin "${GOELEVEN_SLOT_PASSWORD}" -y cert -w "${GOELEVEN_LABEL}.der"
+    pkcs11-tool --module ${GOELEVEN_HSMLIB} --label "${GOELEVEN_LABEL}" --id 01 --login --pin "${GOELEVEN_SLOT_PASSWORD}" --type cert -w "${GOELEVEN_LABEL}.der"
 fi
-openssl x509 -in "/opt/goeleven/${GOELEVEN_LABEL}.der" -inform der -outform pem -out "/opt/goeleven/${GOELEVEN_LABEL}.crt"
-openssl x509 -in "/opt/goeleven/${GOELEVEN_LABEL}.crt" -text
+
+# export the cert into a useful form
+if [ \! -e "/softhsm/${GOELEVEN_LABEL}.crt" ] ; then
+    echo "Export certificate to /softhsm/${GOELEVEN_LABEL}.crt"
+    pkcs11-tool ${GOELEVEN_HSMLIB} --write-object 01 --type cert --output-file "/opt/goeleven/${GOELEVEN_LABEL}.der"
+    openssl x509 -in "/opt/goeleven/${GOELEVEN_LABEL}.der" -inform der -outform pem -out "/softhsm/${GOELEVEN_LABEL}.crt"
+    openssl x509 -in "/softhsm/${GOELEVEN_LABEL}.crt" -text
+fi
 
 cd /opt/goeleven
 exec /opt/goeleven/goeleven
